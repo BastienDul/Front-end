e55e20830b77c97bfcf8bf1fb95877e34e1bc261
Bastien Duliege <bastienduliege@outlook.fr>
2023-09-08 11:05:19 +0200

    Insertion des section dans le js au lieu du html

--- JavaScript/Java_pendu/scripts/script.js
+++ JavaScript/Java_pendu/scripts/script.js
@@ -1,16 +1,29 @@
 console.log("script chargé");
+
+// Création du conteneur main
+let main = document.createElement("main");
+document.body.appendChild(main);
+
+
+// Création du conteneur section qui contiendra la table avec les lettres de l'alphabet
+let sectionAlphabet = document.createElement("section");
+sectionAlphabet.id = "alphabet";
+main.appendChild(sectionAlphabet);
+
 // Creation de la table contenant les buttons
-let sectionAlphabet = document.getElementById("alphabet");
 let uneTable = document.createElement("table");
-uneTable.id="tableau"
+uneTable.id="tableau";
 sectionAlphabet.appendChild(uneTable);
 
 // Creation du tableau contenant les lettres de l'alphabet.
-
 let lettre = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+
+// Création du mot mystère
 const MOTMYSTERE = ["jacuzzi"];
 let lettresMotMystere = MOTMYSTERE[0].split("");
 console.log(lettresMotMystere);
+
+// Création de la boucle afin de stocker les lettre de l'alphabet dans des buttons
 for (let i = 0 ; i <= 25; i++) {
 
     console.log("for chargée")
@@ -29,13 +42,19 @@ for (let i = 0 ; i <= 25; i++) {
 // Création du compteur pour la défaite
 let compteur = 0;
 
-let sectionButton = document.getElementById("button_essais");
+// Creation du conteneur section qui contiendra le boutton pour le décompte d'erreurs.
+let sectionButton = document.createElement("section");
+sectionButton.id = "button_essais";
+main.appendChild(sectionButton);
+
+
+// Création du bouton qui contiendra le decompte des erreurs
 let buttonCompteur = document.createElement("bouton");
 buttonCompteur.id = "button_compteur";
 sectionButton.appendChild(buttonCompteur);
 buttonCompteur.innerText = "A vous de jouer !";
 
-
+// Ecouter les clicks de l'utilisateur afin creer un messages personnaliser a chaque erreur de celui ci !
 buttonCompteur.addEventListener("click",function () {
     compteur++;
 
@@ -69,15 +88,22 @@ switch (compteur) {
 }
 })
 
-// Création des spans pour entre les lettre choisis pas l'utilisateur 
+
+// Creation du conteneur pour acceuillir les element span dedans
+let sectionSpan = document.createElement("section");
+sectionSpan.id = "span_lettre";
+main.appendChild(sectionSpan);
+
+
+// Création des spans pour entrer les lettre choisis pas l'utilisateur 
 for (let i2 = 0; i2 < lettresMotMystere.length ; i2++) {
 
     console.log("span chargé");
 
-
+    
     let span = document.createElement("span");
     span.className = "span_reception" + i2;
-    let sectionSpan = document.getElementById("span_lettre");
+
     sectionSpan.appendChild(span);
     span.textContent= "-" + " ";
 
